31784c9a688df798bc154aabafbdb44c4958342a
anh khoa <anhkhoaquachvo@gmail.com>
2023-05-26 13:31:52 +0700

    '[Set Up] Controller'

--- src/app/tag/tag.controller.ts
+++ src/app/tag/tag.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('-tag')
-export class TagController {}
+@Controller('tag')
+// @ApiTags('tag')
+export class TagController {
+  constructor() { }
+  @Get('/tags')
+  getAll() {
+    return ['title', 'experience'];
+  }
+}
